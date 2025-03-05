42b7c8e772bb849cf37819236725d7fa1fe2b9a5
Kumduy <141148409+Kumduy@users.noreply.github.com>
2025-03-05 22:43:53 +0700

    Update vitimexTest.java

--- src/test/java/vitimexTest.java
+++ src/test/java/vitimexTest.java
@@ -11,50 +11,5 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class vitimexTest {
-    WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-
-        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--user-data-dir=/tmp/chrome-user-data");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
-    @Test
-    public void dangKy() throws InterruptedException {
-        driver.get("https://vitimex.com.vn/dang-ky.html");
-        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("TH01926");
-        driver.findElement(By.xpath("//input[@id='phone']")).sendKeys("0866028127");
-        driver.findElement(By.xpath("//input[@id='pass-regis']")).sendKeys("TH01926");
-        driver.findElement(By.xpath("//input[@id='pass-confirm']")).sendKeys("TH01926");
-        driver.findElement(By.xpath("//a[@id='register-btn']")).click();
-        Thread.sleep(3000);
-        assertTrue(driver.getCurrentUrl().contains("https://vitimex.com.vn/index.php?module=home&view=home"));
-    }
-
-    @Test
-    public void dangNhap() throws InterruptedException {
-        driver.get("https://vitimex.com.vn/");
-        driver.findElement(By.xpath("//div[@class='li_log']//a[@data-bs-toggle='modal']//*[name()='svg']")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//input[@id='account']")).sendKeys("0866028127");
-        driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("TH01926");
-        driver.findElement(By.xpath("//a[@id='signin-btn']")).click();
-        Thread.sleep(3000);
-        assertTrue(driver.getCurrentUrl().contains("https://vitimex.com.vn/"));
-    }
-
 
 }
